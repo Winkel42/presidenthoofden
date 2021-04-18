b7fbc7b8dc23538fb8eb494be53d94501aeae0f0
Winkel42 <82274127+Winkel42@users.noreply.github.com>
2021-04-18 16:07:07 +0200

    Update kaarten.php

--- kaarten.php
+++ kaarten.php
@@ -24,7 +24,75 @@ function is_legaal($kaarten){
 	if(count($kaarten)!=$stapel_breedte && $stapel_breedte!=0){
 		return False;
 	}
-	return True;
+	//kijk of het allemaal dezelfde kaarten en/of jokers zijn, bepaal de waarde
+	$waarde = 'joker';
+	foreach($kaarten as $kaart){
+		if($kaart->waarde != 'joker'){
+			if($kaart->waarde != $waarde && $waarde != 'joker'){
+				return False;
+			}
+			$waarde = $kaart->waarde;
+		}
+	}
+	if($stapel_diepte == 0){
+		return True;
+	}
+	if(count($kaarten) != $stapel_breedte){
+		echo "niet genoeg kaarten";
+		return False;
+	}
+	//check de regels met boeren en jokers
+	$is_er_een_joker = False;
+	foreach($kaarten as $kaart){
+		$is_er_een_joker = ($is_er_een_joker || ($kaart->waarde == 'joker'));
+	}
+	if(($stapel_jokers && $waarde == 'J') || ($stapel_waarde == 'J' && $is_er_een_joker)){
+		return True;
+	}
+	switch($stapel_waarde){
+		case 'lege tafel':
+			return True;
+			break;
+		case 'joker':
+			return ($waarde != 'joker');
+			break;
+		case '8':
+			return !in_array($waarde, Array('joker', '8'));
+			break;
+		case '4':
+			return !in_array($waarde, Array('joker', '8'));
+			break;
+		case '6':
+			return !in_array($waarde, Array('joker', '8', '4'));
+			break;
+		case '7':
+			return in_array($waarde, Array('3', '4', '5', '6'));
+			break;
+		case '3':
+			return !in_array($waarde, Array('joker', '8', '4', '6', '7'));
+			break;
+		case '9':
+			return !in_array($waarde, Array('joker', '8', '4', '6', '7', '3'));
+			break;
+		case '10':
+			return False;
+			break;
+		case 'J':
+			return in_array($waarde, Array('5', 'Q', 'K', 'A', '2'));
+			break;
+		case 'Q':
+			return in_array($waarde, Array('5', 'K', 'A', '2'));
+			break;
+		case 'K':
+			return in_array($waarde, Array('5', 'A', '2'));
+			break;
+		case 'A':
+			return in_array($waarde, Array('5', '2'));
+			break;
+		case '2':
+			return ($waarde == '5');
+			break;
+	}
 }
 
 function is_het_een_ontploffing($kaarten){
@@ -79,7 +147,6 @@ function bepaal_waarde($kaart_id){
 	return $alle_kaarten[$i];
 }
 
-	
 
 class Kaart {
 	public $spel_id;
@@ -95,20 +162,13 @@ class Kaart {
 		$this->waarde = bepaal_waarde($kaart_id);
 		$this->geklikt = 0;
 		//kijk of de kaart al aangeklikt was
-		$sql = "SELECT aangeklikt FROM aangeklikte_kaarten WHERE spel_id=".$spel_id." AND kaart_id=".$this->kaart_id." AND aangeklikt=1";
+		$sql = "SELECT aangeklikt FROM aangeklikte_kaarten WHERE spel_id=".$spel_id." AND kaart_id=".$kaart_id." AND aangeklikt=1";
 		$result = $conn->query($sql);
 		if(!$result){
 			echo $conn->error."<br>".$sql."<br>";
 		}
 		if($result && $result->num_rows>0){
 			$this->geklikt = 1;
-		}
-		else{
-			//stop de kaart in de database
-			$sql = "INSERT INTO aangeklikte_kaarten VALUES(".$spel_id.",".$kaart_id.",0);";
-			if(!$conn->query($sql)){
-				echo $conn->error."<br>".$sql."<br>";
-			}
 		}
 		//kijk of de kaart net aangeklikt werd		
 		$knop_naam = "knop".$spel_id."a".$kaart_id;
@@ -118,6 +178,14 @@ class Kaart {
 			if(!$conn->query($sql)){
 				echo $conn->error."<br>".$sql."<br>";
 			}
+		}
+		//als het de klaver 8 is, is de kaart altijd aangeklikt
+		if($this->kleur == 'klaver' && $this->waarde == '8'){
+			$sql = "UPDATE aangeklikte_kaarten SET aangeklikt=1 WHERE spel_id=".$spel_id." AND kaart_id=".$kaart_id;
+			if(!$conn->query($sql)){
+				echo $conn->error."<br>".$sql."<br>";
+			}
+			$this->geklikt = 1;
 		}
 	}
 	function draw($klikbare_kaarten){
@@ -379,83 +447,90 @@ class Spel {
 		}
 	}
 	function speel($speler, $kaarten){
-	global $stapel_diepte, $conn, $aantal_overgebleven_spelers;
-	$stapel_diepte++;
-	foreach($kaarten as $kaart){
-		//verwijder de kaarten uit de hand
-		$sql = "DELETE FROM spellen_spelers_kaarten WHERE spel_id=".$this->spel_id." AND speler_id=".$speler." AND kaart_id=".$kaart->kaart_id;
-		if(!$conn->query($sql)){
-			echo $conn->error."<br>".$sql."<br>";
-		}
-		//verwijder ze ook als aangeklikt
-		$sql = "DELETE FROM aangeklikte_kaarten WHERE spel_id=".$this->spel_id." AND kaart_id=".$kaart->kaart_id;
-		if(!$conn->query($sql)){
-			echo $conn->error."<br>".$sql."<br>";
-		}
-		//stop ze in de stapel
-		$sql = "INSERT INTO spel_stapel (spel_id, kaart_id, diepte) VALUES (".$this->spel_id.",".$kaart->kaart_id.",".$stapel_diepte.")";					
-		if(!$conn->query($sql)){
-			echo $conn->error."<br>".$sql."<br>";
-		}
-	}
-	
-	
-	//kijk of de speler nu uit is
-	$sql = "SELECT kaart_id FROM spellen_spelers_kaarten WHERE spel_id=".$this->spel_id." AND speler_id=".$speler;
-	$result = $conn->query($sql);
-	if(!$result){
-		echo $conn->error."<br>".$sql."<br>";
-	}
-	$klaar = ($result->num_rows == 0);
-	//kijk of er net ontploft is
-	$ontploft = is_het_een_ontploffing($kaarten);
-	
-	if($ontploft){
-		//kijk of er vier klaver negens bij zijn
-		$aantal_klaver_negens = 0;
+		global $stapel_diepte, $conn, $aantal_overgebleven_spelers;
+		$stapel_diepte++;
 		foreach($kaarten as $kaart){
-			if($kaart->waarde == '9' && $kaart->kleur == 'klaver'){
-				$aantal_klaver_negens++;
-			}
-		}
-		if($aantal_klaver_negens == 4){
-			//de persoon heeft direct gewonnen
-			$sql = "UPDATE spellen_spelers SET stand = stand + 1 WHERE spel_id=".$this->spel_id." AND stand!=0 AND stand<=(
-						SELECT MIN(stand) FROM spellen_spelers WHERE spel_id=".$this->spel_id." AND stand+1 NOT IN (
-							SELECT stand FROM spellen_spelers WHERE spel_id = ".$this->spel_id."
-						)
-					)";
-			if(!$conn->query($sql)){
-				echo $conn->error."<br>".$sql."<br>";
-			}		
-			$sql = "UPDATE spellen_spelers SET stand = 1 WHERE spel_id=".$this->spel_id." AND speler_id=".$speler;
+			//verwijder de kaarten uit de hand
+			$sql = "DELETE FROM spellen_spelers_kaarten WHERE spel_id=".$this->spel_id." AND speler_id=".$speler." AND kaart_id=".$kaart->kaart_id;
 			if(!$conn->query($sql)){
 				echo $conn->error."<br>".$sql."<br>";
 			}
-			return;
+			//verwijder ze ook als aangeklikt
+			$sql = "DELETE FROM aangeklikte_kaarten WHERE spel_id=".$this->spel_id." AND kaart_id=".$kaart->kaart_id;
+			if(!$conn->query($sql)){
+				echo $conn->error."<br>".$sql."<br>";
+			}
+			//stop ze in de stapel
+			$sql = "INSERT INTO spel_stapel (spel_id, kaart_id, diepte) VALUES (".$this->spel_id.",".$kaart->kaart_id.",".$stapel_diepte.")";					
+			if(!$conn->query($sql)){
+				echo $conn->error."<br>".$sql."<br>";
+			}
 		}
 		
-		//de persoon blijft aan de beurt en mag straks de stapel weghalen, maar dat staat ergens anders
+		
+		//kijk of de speler nu uit is
+		$sql = "SELECT kaart_id FROM spellen_spelers_kaarten WHERE spel_id=".$this->spel_id." AND speler_id=".$speler;
+		$result = $conn->query($sql);
+		if(!$result){
+			echo $conn->error."<br>".$sql."<br>";
+		}
+		$klaar = ($result->num_rows == 0);
+		//kijk of er net ontploft is
+		$ontploft = is_het_een_ontploffing($kaarten);
+		
+		if($ontploft){
+			//kijk of er vier klaver negens bij zijn
+			$aantal_klaver_negens = 0;
+			foreach($kaarten as $kaart){
+				if($kaart->waarde == '9' && $kaart->kleur == 'klaver'){
+					$aantal_klaver_negens++;
+				}
+			}
+			if($aantal_klaver_negens == 4){
+				//de persoon heeft direct gewonnen
+				$sql = "UPDATE spellen_spelers SET stand = stand + 1 WHERE spel_id=".$this->spel_id." AND stand!=0 AND stand<=(
+							SELECT MIN(stand) FROM spellen_spelers WHERE spel_id=".$this->spel_id." AND stand+1 NOT IN (
+								SELECT stand FROM spellen_spelers WHERE spel_id = ".$this->spel_id."
+							)
+						)";
+				if(!$conn->query($sql)){
+					echo $conn->error."<br>".$sql."<br>";
+				}		
+				$sql = "UPDATE spellen_spelers SET stand = 1 WHERE spel_id=".$this->spel_id." AND speler_id=".$speler;
+				if(!$conn->query($sql)){
+					echo $conn->error."<br>".$sql."<br>";
+				}
+				return;
+			}
+			
+			//de persoon blijft aan de beurt en mag straks de stapel weghalen, maar dat staat ergens anders
+		}
+		
+		//ook nog de beurtvolgorde aanpassen
+		if(!$ontploft){
+			$this->volgende_aan_de_beurt();
+		}
+		
+		
+		if($klaar && !$ontploft){
+			//zet de stand in de database
+			//we halen de speler pas later uit de beurtvolgorde, want misschien heeft diegene nog een stapel
+			$sql = "UPDATE spellen_spelers SET stand=
+			(SELECT MIN(stand) FROM spellen_spelers WHERE spel_id=".$this->spel_id." AND stand+1 NOT IN(SELECT stand FROM spellen_spelers WHERE spel_id=".$this->spel_id."))
+			+1 WHERE spel_id=".$this->spel_id." AND speler_id=".$speler;
+			if(!$conn->query($sql)){
+				echo $conn->error."<br>".$sql."<br>";
+			}
+		}
 	}
-	
-	//ook nog de beurtvolgorde aanpassen
-	if(!$ontploft){
-		$this->volgende_aan_de_beurt();
-	}
-	
-	
-	if($klaar && !$ontploft){
-		//zet de stand in de database
-		//we halen de speler pas later uit de beurtvolgorde, want misschien heeft diegene nog een stapel
-		$sql = "UPDATE spellen_spelers SET stand=
-		(SELECT MIN(stand) FROM spellen_spelers WHERE spel_id=".$this->spel_id." AND stand+1 NOT IN(SELECT stand FROM spellen_spelers WHERE spel_id=".$this->spel_id."))
-		+1 WHERE spel_id=".$this->spel_id." AND speler_id=".$speler;
+	function verwijder_geklikte_kaarten($speler){
+		global $conn;
+		$sql = "UPDATE aangeklikte_kaarten, spellen_spelers_kaarten SET aangeklikt=0 WHERE aangeklikte_kaarten.spel_id = spellen_spelers_kaarten.spel_id
+				AND aangeklikte_kaarten.spel_id=".$this->spel_id." AND aangeklikte_kaarten.kaart_id=spellen_spelers_kaarten.kaart_id AND speler_id=".$speler;
 		if(!$conn->query($sql)){
 			echo $conn->error."<br>".$sql."<br>";
 		}
 	}
-}
-
 }
 	
 
@@ -492,13 +567,17 @@ function begin_spel(){
 		}
 	}
 	//creeer het spel in de database
-	$sql = "INSERT INTO actieve_spellen (kamer_id, spel_id) VALUES('".$kamer_id."','".$spel_id."');";
-	if($conn->query($sql) == FALSE){
-		echo 'database updaten mislukt';
-		echo $conn->error;
+	$sql = "INSERT INTO actieve_spellen  VALUES(".$kamer_id.",".$spel_id.");";
+	if(!$conn->query($sql)){
+		echo $conn->error."<br>".$sql."<br>";
 	}	
-	
-	
+	//creer de kaarten in de andere database
+	foreach(range(0,63) as $kaart_id){
+		$sql = "INSERT INTO aangeklikte_kaarten VALUES(".$spel_id.",".$kaart_id.",0)";
+		if(!$conn->query($sql)){
+			echo $conn->error."<br>".$sql;
+		}
+	}	
 	//nu gaan we de kaarten uitdelen
 	$kaart_ids = range(0, 63);
 	shuffle($kaart_ids);
@@ -713,6 +792,11 @@ function ververs(){
 		}
 	}
 	else{
+		$nieuw_spel_knop_naam = "opnieuwdelen".$kamer_id;
+		if(isset($_POST[$nieuw_spel_knop_naam]) && $_POST[$nieuw_spel_knop_naam] != "doe niets"){
+			begin_spel();
+			return ververs();
+		}
 		teken_zonder_spel();
 		return False;
 	}
@@ -727,6 +811,12 @@ function ververs(){
 	//test:
 	//echo "de diepte is ".$stapel_diepte.", de breedte is ".$stapel_breedte.", de waarde is ".$stapel_waarde." en er zijn jokers: ".($stapel_jokers?"ja":"nee")."<br> er zijn ".$aantal_spelers_in_spel." spelers over<br>";
 	
+	//iedereen die niet aan de beurt is heeft geen kaarten meer aangeklikt
+	foreach($spel->spelers as $speler){
+		if($speler != $speler_aan_de_beurt){
+			$spel->verwijder_geklikte_kaarten($speler);
+		}
+	}	
 	
 	//nu moeten we nog kijken, of er iets moet gebeuren, omdat iemand op een knop gedrukt heeft
 	$knop_naam = "grote_knop_van_".$speler_aan_de_beurt;
